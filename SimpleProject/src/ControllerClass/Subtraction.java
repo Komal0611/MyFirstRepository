83b34efee109b0fcd027602fa71f806d94e3432d
Arpitha <Arpitha@DESKTOP-J390HKU>
2023-02-20 14:59:11 +0530

    commiting branch x

---
+++ SimpleProject/src/ControllerClass/Subtraction.java
@@ -0,0 +1,13 @@
+package ControllerClass;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+ 
+		int a=10;
+		int b=15;
+		int result=b-a;
+		System.out.println(result);
+	}
+
+}
